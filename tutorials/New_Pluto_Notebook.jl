99fc290e7f84628b38524b2a1303211c684577cf
Sashedher J <sashedher.14@gmail.com>
2021-03-30 09:46:16 +0530

    File to load new notebook
    note book to load with some useful packages

---
+++ tutorials/New_Pluto_Notebook.jl
@@ -0,0 +1,35 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7420c480-910c-11eb-2338-6bbd68005f0a
+begin
+    using Pkg
+    Pkg.add("PlutoUI")
+    Pkg.add("FreqTables")
+    Pkg.add("RDatasets")
+    Pkg.add("StatsBase")
+    Pkg.add("StatsPlots")
+    Pkg.add("Distributions")
+    Pkg.add("Statistics")
+    Pkg.add("LaTeXStrings")
+end
+
+# ╔═╡ 298ff14c-910d-11eb-1088-efa7ab843b4f
+begin
+    using PlutoUI
+    using FreqTables
+    using RDatasets
+    using StatsBase
+    using StatsPlots
+    using Distributions
+    using Statistics
+    using LaTeXStrings
+end
+
+
+# ╔═╡ Cell order:
+# ╠═7420c480-910c-11eb-2338-6bbd68005f0a
+# ╠═298ff14c-910d-11eb-1088-efa7ab843b4f
